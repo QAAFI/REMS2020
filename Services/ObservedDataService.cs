06996e55c739eb6dcead3d8c6f6120d533ddddf9
Jason Brider <jason.brider@gmail.com>
2019-11-12 13:41:31 +1000

    adding service to write observed data

---
+++ Services/ObservedDataService.cs
@@ -0,0 +1,18 @@
+﻿using REMS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public static class ObservedDataService
+    {
+        public static void WriteDailyObservedData(this IREMSDatabase db, string filepath)
+        {
+            //Experiment Treatment Plot
+            //list of output variables
+            this.
+        }
+
+    }
+}
