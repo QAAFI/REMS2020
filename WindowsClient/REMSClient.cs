a8873ab33cab74192aadf000dc02892339a9a9f7
Jason Brider <jason.brider@gmail.com>
2019-11-13 09:25:58 +1000

    Add msg to show export is complete

--- WindowsClient/REMSClient.cs
+++ WindowsClient/REMSClient.cs
@@ -305,6 +305,7 @@ namespace WindowsClient
                     database.GenerateMetFiles(path);
 
                     Application.UseWaitCursor = false;
+                    MessageBox.Show($"Export Complete.");
                 }
                 catch (Exception error)
                 {
