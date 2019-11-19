12879159def5995cb9f0692f464fe5a07eaafdd4
Michael Stower <m.stower@uq.edu.au>
2019-11-19 11:06:09 +1000

    Added a property mapping class
    This class is intended to smooth the process of converting property/trait names to their ApsimX equivalent, but has been implemented generically.

---
+++ Src/Core/Application/Common/Mappings/IPropertyMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rems.Application.Common.Models;
+
+namespace Rems.Application.Common.Mappings
+{
+    /// <summary>
+    /// Contains a collection of parameters that may be mapped to some other value
+    /// </summary>
+    public interface IPropertyMap : IEquatable<IPropertyMap>, IEnumerable<KeyValuePair<string, string>>
+    {
+        /// <summary>
+        /// The name of the mapping
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Check if the property is mapped
+        /// </summary>
+        bool IsMapped(string property);
+
+        /// <summary>
+        /// Add a new property to the mapping
+        /// </summary>
+        void AddMapping(string property);
+
+        /// <summary>
+        /// Remove a property from the mapping
+        /// </summary>
+        bool RemoveMapping(string property);
+        
+        /// <summary>
+        /// Get or Set the value the property is mapped to
+        /// </summary>
+        string this[string property] { get; set; }
+
+    }
+}
