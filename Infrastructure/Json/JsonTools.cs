e799e52a1458e5c00fb39ca13555fd69c62c830b
Michael Stower <m.stower@uq.edu.au>
2019-11-19 11:15:46 +1000

    The ability to read/write objects as Json files was added to the infrastructure project.

---
+++ Infrastructure/Json/JsonTools.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Rems.Infrastructure.Json
+{
+    public static class JsonTools
+    {
+        public static T LoadJson<T>(string file)
+        {
+            using var stream = new StreamReader(file);
+            using var reader = new JsonTextReader(stream);
+
+            var serializer = new JsonSerializer()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            return serializer.Deserialize<T>(reader);
+        }
+
+        public static void SaveJson(string file, object json)
+        {
+            using var stream = new StreamWriter(file);
+            using var writer = new JsonTextWriter(stream)
+            {
+                CloseOutput = true,
+                AutoCompleteOnClose = true
+            };
+
+            var serializer = new JsonSerializer()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            serializer.Serialize(writer, json);
+        }
+    }
+}
