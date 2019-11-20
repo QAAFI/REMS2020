b3e5aca935c442c4c8a07de2b99c33ff7abb0a40
Michael Stower <m.stower@uq.edu.au>
2019-11-20 15:55:45 +1000

    Added SoilLayerDepth query

---
+++ Src/Core/Application/CQRS/Soils/Queries/SoilLayerDepthQuery/SoilLayerDepthQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Rems.Application.Common.Interfaces;
+
+namespace Rems.Application.Soils.Queries.x
+{
+    public class SoilLayerDepthQueryHandler : IRequestHandler<SoilLayerDepthQuery, string[]>
+    {
+        private readonly IRemsDbContext _context;
+
+        public SoilLayerDepthQueryHandler(IRemsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string[]> Handle(SoilLayerDepthQuery request, CancellationToken cancellationToken)
+        {
+            return (from layer in _context.SoilLayers
+                   where layer.SoilId == request.SoilId
+                   orderby layer.DepthFrom
+                   select $"({layer.DepthFrom ?? 0}-{layer.DepthTo ?? 0}").ToArray();
+        }
+    }
+}
