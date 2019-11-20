61660ced2f814c9068f7201b93f6596b5ad2329c
Michael Stower <m.stower@uq.edu.au>
2019-11-20 14:02:49 +1000

    Datafiles have been moved to the Infrastructure layer

---
+++ Infrastructure/DataFiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Rems.Infrastructure
+{
+    public static class DataFiles
+    {
+        public static string ReadRawText(string file)
+        {
+            var filePath = Path.Combine("Data", "apsimx", file);
+            StringBuilder builder = new StringBuilder();
+            using var reader = new StreamReader(filePath);
+            while (!reader.EndOfStream)
+            {
+                builder.AppendLine(reader.ReadLine());
+            };
+            builder.Replace("\r", "");
+            return builder.ToString();
+        }
+    }
+}
