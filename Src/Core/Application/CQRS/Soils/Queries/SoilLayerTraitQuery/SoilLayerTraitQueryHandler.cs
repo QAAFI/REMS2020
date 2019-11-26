ff40f7a211dac58db1dad30ce8a34d2aa73ba385
Michael Stower <m.stower@uq.edu.au>
2019-11-26 13:16:40 +1000

    Replaced SoilLayerDataQuery with SoilLayerTraitQuery
    The previous query was not targeted at the correct data within Rems for its intended use in Apsim. The ApsimBuilder now correctly queries the soil layer traits instead of raw layer data.

---
+++ Src/Core/Application/CQRS/Soils/Queries/SoilLayerTraitQuery/SoilLayerTraitQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+
+using Rems.Application.Common.Interfaces;
+
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Rems.Application.Soils.Queries
+{
+    public class SoilLayerTraitQueryHandler : IRequestHandler<SoilLayerTraitQuery, double[]>
+    {
+        private readonly IRemsDbContext _context;
+        private readonly IMediator _mediator;
+
+        public SoilLayerTraitQueryHandler(IRemsDbFactory factory, IMediator mediator)
+        {
+            _context = factory.Context;
+            _mediator = mediator;
+        }
+
+        public async Task<double[]> Handle(SoilLayerTraitQuery request, CancellationToken cancellationToken)
+        {
+            var trait = _context.Traits.FirstOrDefault(t => t.Name == request.TraitName);
+
+            var layers = from layer in _context.SoilLayers
+                         where layer.SoilId == request.SoilId       // Find the layers of the soil
+                         orderby layer.DepthFrom                    // Sort the layers by depth
+                         select (
+                            from traits in layer.SoilLayerTraits    // Find all the traits in the soil layer
+                            where traits.Trait == trait             // Filter by the requested trait
+                            select traits.Value.Value               // Select the value of the trait
+                         ).FirstOrDefault();                        // Return 0.0 if no value is found
+            
+            return layers.ToArray();
+        }
+    }
+}
