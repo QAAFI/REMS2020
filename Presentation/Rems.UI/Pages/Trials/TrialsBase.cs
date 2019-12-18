a95bb70dc7d9767c922cd2c849383a98b850100d
Jason <Brider>
2019-12-18 16:15:30 +1000

    getting blazor server side working

---
+++ Presentation/Rems.UI/Pages/Trials/TrialsBase.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Rems.Application.Experiments.Queries.GetExperimentList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rems.UI.Pages.Trials
+{
+    public class TrialsBase : ComponentBase
+    {
+        [Inject]
+        public IMediator Mediator { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var selections = await this.Mediator.Send(new GetExperimentsListQuery());
+            //this.Model = new WebApiStyleViewModel(selections.Roles, selections.Names);
+        }
+
+    }
+}
