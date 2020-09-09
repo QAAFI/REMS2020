8c6ae5c2271ba6b993a14b2bf34aef5156536dc9
Michael Stower <m.stower@uq.edu.au>
2020-09-09 11:13:27 +1000

    Changed SetValue to an entity extension
    Was previously a private method in the BulkInsertCommand

--- Core/Application/Common/Extensions/EntityExtensions.cs
+++ Core/Application/Common/Extensions/EntityExtensions.cs
@@ -46,5 +46,29 @@ namespace Rems.Application.Common.Extensions
                 if (info.GetValue(entity)?.ToString() == value.ToString()) return true;
             return false;
         }
+
+        public static void SetValue(this IEntity entity, PropertyInfo info, object value)
+        {
+            // Entity will use default values for null or 0 entries
+            if (value is DBNull) return;
+            if (value is double d && d == 0) return;
+
+            var type = info.PropertyType;
+
+            // Handle strings
+            if (type == typeof(string))
+            {
+                info.SetValue(entity, value);
+                return;
+            }
+            // Handle value types
+            else
+            {
+                var v = value.ConvertDBValue(type);
+                info.SetValue(entity, v);
+            }
+
+            return;
+        }
     }
 }
