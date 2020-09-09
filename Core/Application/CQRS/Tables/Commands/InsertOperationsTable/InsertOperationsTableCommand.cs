33d73253a50ab71f7525071718a21840fec1fdd7
Michael Stower <m.stower@uq.edu.au>
2020-09-09 11:44:01 +1000

    Created InsertOperationsTable command
    Intended to separate BulkInsertCommand into simpler commands

---
+++ Core/Application/CQRS/Tables/Commands/InsertOperationsTable/InsertOperationsTableCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Rems.Application.CQRS
+{
+    public class InsertOperationsTableCommand : IRequest<Unit>
+    {
+        public DataTable Table { get; set; }
+
+        public Type Type { get; set; }
+    }
+}
