548093fe8229c2c0d5a645f21db80dd20828e477
Michael Stower <m.stower@uq.edu.au>
2020-09-09 11:39:13 +1000

    Moved FindEntity into EntityTypesQuery
    Intended to separate BulkInsertCommand into simpler commands

---
+++ Core/Application/CQRS/Entities/Queries/EntityTypes/EntityTypesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rems.Application.CQRS
+{
+    public class EntityTypesQuery : IRequest<Type>
+    {
+        public string Name { get; set; }
+    }
+}
