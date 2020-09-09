4a1dd1ed1eee4d7c4221e9a1e92a1253afe8bd02
Michael Stower <m.stower@uq.edu.au>
2020-09-09 11:43:36 +1000

    Created InsertSoilLayerTable command
    Intended to separate BulkInsertCommand into simpler commands

---
+++ Core/Application/CQRS/SoilLayerData/Commands/InsertSoilLayerTable/InsertSoilLayerTableCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Rems.Application.CQRS
+{
+    public class InsertSoilLayerTableCommand : IRequest<Unit>
+    {
+        public DataTable Table { get; set; }
+
+        public int Skip { get; set; }
+
+        public string Type { get; set; }
+    }
+}
