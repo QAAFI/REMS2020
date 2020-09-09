8d6a002c94066bf4364d39c842712fa1cde4a6ea
Michael Stower <m.stower@uq.edu.au>
2020-09-09 11:41:35 +1000

    Created InsertMetDataTable command
    Intended to separate BulkInsertCommand into simpler commands

---
+++ Core/Application/CQRS/Met/Commands/InsertMetDataTable/InsertMetDataTableCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Rems.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Rems.Application.CQRS
+{
+    public class InsertMetDataTableCommand : IRequest<Unit>
+    {
+        public DataTable Table { get; set; }
+
+        public int Skip { get; set; }
+
+        public string Type { get; set; }
+    }
+}
