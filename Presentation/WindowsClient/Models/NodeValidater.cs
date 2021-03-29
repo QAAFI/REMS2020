3d59d11a77cabd8754c8657cc2d6ab805fcea17a
Michael Stower <m.stower@uq.edu.au>
2021-03-29 12:13:30 +1000

    Created a base node validator to help with object disposal
    Working on #72

--- Presentation/WindowsClient/Models/NodeValidater.cs
+++ Presentation/WindowsClient/Models/NodeValidater.cs
@@ -2,6 +2,7 @@
 using Rems.Application.CQRS;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -44,21 +45,61 @@ namespace WindowsClient.Models
         DataNode CreateColumnNode(int i, Func<object, Task<object>> query);
     }
 
-    /// <summary>
-    /// A default implementation of <see cref="INodeValidater"/> that always validates
-    /// </summary>
-    public class NullValidater : INodeValidater
+    public abstract class BaseValidater<TDisposable> : INodeValidater, IDisposable
+        where TDisposable : IDisposable
     {
+        private bool disposedValue;
+
         /// <inheritdoc/>
         public event Action<string, object> StateChanged;
 
         /// <inheritdoc/>
         public event Action<Advice> SetAdvice;
 
-        /// <inheritdoc/>
-        public async Task Validate()
+        public TDisposable Component { get; set; }
+
+        protected void InvokeStateChanged(string state, object value)
+            => StateChanged?.Invoke(state, value);
+
+        protected void InvokeSetAdvice(Advice advice)
+            => SetAdvice?.Invoke(advice);
+
+        public abstract Task Validate();
+
+        protected virtual void Dispose(bool disposing)
         {
-            StateChanged?.Invoke("Valid", true);
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Component.Dispose();
+                }
+
+                StateChanged = null;
+                SetAdvice = null;
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    /// <summary>
+    /// A default implementation of <see cref="INodeValidater"/> that always validates
+    /// </summary>
+    public class NullValidater<TComponent> : BaseValidater<TComponent>
+        where TComponent : MarshalByValueComponent
+    {
+        /// <inheritdoc/>
+        public async override Task Validate()
+        {
+            InvokeStateChanged("Valid", true);
         }
     }
 
@@ -67,61 +108,53 @@ namespace WindowsClient.Models
     /// <summary>
     /// A simple implementation of <see cref="ITableValidater"/>, for generic excel table data
     /// </summary>
-    public class TableValidater : ITableValidater
+    public class TableValidater : BaseValidater<DataTable>, ITableValidater
     {
-        /// <inheritdoc/>
-        public event Action<string, object> StateChanged;
-
-        /// <inheritdoc/>
-        public event Action<Advice> SetAdvice;
-
-        readonly DataTable table;
-
         public TableValidater(DataTable table)
         {
-            this.table = table;
+            Component = table;
         }
 
         /// <inheritdoc/>
-        public async Task Validate()
+        public async override Task Validate()
         {
-            var valid = table.Columns
+            var valid = Component.Columns
                 .Cast<DataColumn>()
                 .Select(c => (bool)c.ExtendedProperties["Valid"] || (bool)c.ExtendedProperties["Ignore"])
                 .Aggregate((v1, v2) => v1 &= v2);
-
+            
             // A table is valid if all of its columns are valid or ignored
             if (valid)
             {
-                StateChanged?.Invoke("Valid", true);
-                StateChanged?.Invoke("Override", "");
+                InvokeStateChanged("Valid", true);
+                InvokeStateChanged("Override", "");
 
                 var advice = new Advice();
                 advice.Include("This table is valid. Check the other tables prior to import.", Color.Black);
 
-                SetAdvice?.Invoke(advice);
+                InvokeSetAdvice(advice);
             }
             else
             {
-                StateChanged?.Invoke("Valid", false);
-                StateChanged?.Invoke("Override", "Warning");
+                InvokeStateChanged("Valid", false);
+                InvokeStateChanged("Override", "Warning");
 
                 var advice = new Advice();
                 advice.Include("This table contains columns that REMS does not recognise." +
                     " Please fix the columns before importing", Color.Black);
-                SetAdvice?.Invoke(advice);
+                InvokeSetAdvice(advice);
             }
         }
 
         /// <inheritdoc/>
         public DataNode CreateColumnNode(int i, Func<object, Task<object>> query)
         {
-            var col = table.Columns[i];
+            var col = Component.Columns[i];
             var excel = new ExcelColumn(col);
             var validater = new ColumnValidater(col);
 
             validater.Query += (o) => query?.Invoke(o);            
-            validater.SetAdvice += a => SetAdvice?.Invoke(a);
+            validater.SetAdvice += a => InvokeSetAdvice(a);
 
             return new DataNode(excel, validater);
         }
@@ -130,40 +163,32 @@ namespace WindowsClient.Models
     /// <summary>
     /// Implements <see cref="ITableValidater"/> with handling for specialised tables
     /// </summary>
-    public class CustomTableValidater : ITableValidater
+    public class CustomTableValidater : BaseValidater<DataTable>, ITableValidater
     {
-        /// <inheritdoc/>
-        public event Action<Advice> SetAdvice;
-
-        /// <inheritdoc/>
-        public event Action<string, object> StateChanged;
-
         public readonly string[] columns;
-
-        protected readonly DataTable table;
 
         public CustomTableValidater(DataTable table, string[] columns)
         {
-            this.table = table;
+            Component = table;
             this.columns = columns;
         }
 
         /// <inheritdoc/>
-        public async Task Validate()
+        public async override Task Validate()
         {
             bool valid = true;
 
-            foreach (DataColumn col in table.Columns)
+            foreach (DataColumn col in Component.Columns)
                 valid &= (bool)col.ExtendedProperties["Valid"];
 
             if (valid)
             {
-                StateChanged?.Invoke("Valid", true);
-                StateChanged?.Invoke("Override", "");
+                InvokeStateChanged("Valid", true);
+                InvokeStateChanged("Override", "");
 
                 var advice = new Advice();
                 advice.Include("Ready for import.", Color.Black);
-                SetAdvice?.Invoke(advice);
+                InvokeSetAdvice(advice);
             }
             else
             {
@@ -175,7 +200,7 @@ namespace WindowsClient.Models
                 {
                     Color color;
 
-                    string name = table.Columns[i].ColumnName;
+                    string name = Component.Columns[i].ColumnName;
                     string need = columns[i];
 
                     if (name == need)
@@ -188,9 +213,9 @@ namespace WindowsClient.Models
 
                 advice.Include("\nRight-click nodes to see options.", Color.Black);
 
-                SetAdvice?.Invoke(advice);
-                StateChanged?.Invoke("Valid", false);
-                StateChanged?.Invoke("Override", "Warning");
+                InvokeSetAdvice(advice);
+                InvokeStateChanged("Valid", false);
+                InvokeStateChanged("Override", "Warning");
             }
                 
         }
@@ -198,14 +223,14 @@ namespace WindowsClient.Models
         /// <inheritdoc/>
         public DataNode CreateColumnNode(int i, Func<object, Task<object>> query)
         {
-            var col = table.Columns[i];
+            var col = Component.Columns[i];
             var excel = new ExcelColumn(col);
 
             INodeValidater validater = (i < columns.Length) ? 
                 new OrdinalValidater(col, i, columns[i]) as INodeValidater : 
-                new NullValidater();
+                new NullValidater<MarshalByValueComponent>();
             
-            validater.SetAdvice += a => SetAdvice?.Invoke(a);
+            validater.SetAdvice += a => InvokeSetAdvice(a);
 
             return new DataNode(excel, validater);
         }
@@ -217,31 +242,23 @@ namespace WindowsClient.Models
     /// <summary>
     /// A simple implementation of <see cref="INodeValidater"/> for generic columns of excel data
     /// </summary>
-    public class ColumnValidater : INodeValidater
+    public class ColumnValidater : BaseValidater<DataColumn>
     {
         public event Func<object, Task<object>> Query;
         private async Task<T> InvokeQuery<T>(IRequest<T> query) => (T)await Query(query);
 
-        /// <inheritdoc/>
-        public event Action<string, object> StateChanged;
-
-        /// <inheritdoc/>
-        public event Action<Advice> SetAdvice;
-
-        readonly DataColumn column;
-
         public ColumnValidater(DataColumn column)
         {
-            this.column = column;
+            Component = column;
         }
 
         /// <inheritdoc/>
-        public async Task Validate()
+        public async override Task Validate()
         {
             // If the colum node is not valid for import, update the state to warn the user
-            if (column.ExtendedProperties["Info"] is null && !await IsTrait())
+            if (Component.ExtendedProperties["Info"] is null && !await IsTrait())
             {
-                StateChanged?.Invoke("Valid", false);
+                InvokeStateChanged("Valid", false);
 
                 var advice = new Advice();
                 advice.Include("The type of column could not be determined. ", Color.Black);
@@ -254,17 +271,17 @@ namespace WindowsClient.Models
                 advice.Include("Set property\n", Color.Blue);
                 advice.Include("    - Match the column to a REMS property\n", Color.Black );
 
-                SetAdvice?.Invoke(advice);
+                InvokeSetAdvice(advice);
             }
             else
             {
-                StateChanged?.Invoke("Valid", true);
-                StateChanged?.Invoke("Override", "");
+                InvokeStateChanged("Valid", true);
+                InvokeStateChanged("Override", "");
 
                 var advice = new Advice();
                 advice.Include("Ready to be imported.\n", Color.Black);
 
-                SetAdvice?.Invoke(advice);
+                InvokeSetAdvice(advice);
             }
         }
 
@@ -275,51 +292,37 @@ namespace WindowsClient.Models
         {
             return
                     // Test if the trait is in the database
-                    await InvokeQuery(new TraitExistsQuery() { Name = column.ColumnName })
+                    await InvokeQuery(new TraitExistsQuery() { Name = Component.ColumnName })
                     // Or in the spreadsheet traits table
-                    || column.Table.DataSet.Tables["Traits"] is DataTable traits
+                    || Component.Table.DataSet.Tables["Traits"] is DataTable traits
                     // If it is, find the column of trait names
                     && traits.Columns["Name"] is DataColumn name
                     // 
-                    && traits.Rows.Cast<DataRow>().Any(r => r[name].ToString().ToLower() == column.ColumnName.ToLower());
+                    && traits.Rows.Cast<DataRow>().Any(r => r[name].ToString().ToLower() == Component.ColumnName.ToLower());
         }
     }
 
     /// <summary>
     /// Implements <see cref="INodeValidater"/> for ordinal columns
     /// </summary>
-    public class OrdinalValidater : INodeValidater
+    public class OrdinalValidater : BaseValidater<DataColumn>
     {
-        /// <inheritdoc/>
-        public event Action<string, object> StateChanged;
-
-        /// <inheritdoc/>
-        public event Action<Advice> SetAdvice;
-
-        readonly DataColumn col;
         readonly int ordinal;
         readonly string name;
 
         public OrdinalValidater(DataColumn column, int o, string n)
         {
-            col = column;
+            Component = column;
             ordinal = o;
             name = n;
         }
 
         /// <inheritdoc/>
-        public async Task Validate()
+        public async override Task Validate()
         {
-            if (col.Ordinal == ordinal && col.ColumnName == name)
-            {
-                StateChanged?.Invoke("Valid", true);
-                StateChanged?.Invoke("Override", "");
-            }
-            else
-            {
-                StateChanged?.Invoke("Valid", false);
-                StateChanged?.Invoke("Override", "Warning");
-            }
+            bool valid = Component.Ordinal == ordinal && Component.ColumnName == name;
+            InvokeStateChanged("Valid", valid);
+            InvokeStateChanged("Override", valid ? "" : "Warning");
         }
     }
 
