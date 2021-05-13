953821a55657c0a32c41a3cbed5305f9a80a6fb2
MikeStower <michaelostower@gmail.com>
2021-05-13 13:27:14 +1000

    Updated controls to .NET 5.0

--- Presentation/WindowsClient/Controls/Experiments/ExperimentDetailer.Designer.cs
+++ Presentation/WindowsClient/Controls/Experiments/ExperimentDetailer.Designer.cs
@@ -32,16 +32,16 @@
             this.experimentsTree = new System.Windows.Forms.TreeView();
             this.experimentDetails = new System.Windows.Forms.TabControl();
             this.pageSummary = new System.Windows.Forms.TabPage();
+            this.summariser = new WindowsClient.Controls.Experiments.ExperimentSummariser();
             this.pageDesign = new System.Windows.Forms.TabPage();
             this.designBox = new System.Windows.Forms.TextBox();
             this.designData = new System.Windows.Forms.DataGridView();
             this.pageOperation = new System.Windows.Forms.TabPage();
-            this.pageData = new System.Windows.Forms.TabPage();
-            this.pageSoil = new System.Windows.Forms.TabPage();
             this.operations = new WindowsClient.Controls.OperationsChart();
+            this.pageData = new System.Windows.Forms.TabPage();
             this.traitChart = new WindowsClient.Controls.TraitChart();
+            this.pageSoil = new System.Windows.Forms.TabPage();
             this.soilsChart = new WindowsClient.Controls.SoilChart();
-            this.summariser = new WindowsClient.Controls.Experiments.ExperimentSummariser();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -59,6 +59,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -68,8 +69,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.experimentDetails);
-            this.splitContainer1.Size = new System.Drawing.Size(828, 557);
-            this.splitContainer1.SplitterDistance = 173;
+            this.splitContainer1.Size = new System.Drawing.Size(966, 643);
+            this.splitContainer1.SplitterDistance = 201;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 5;
             // 
             // experimentsTree
@@ -77,8 +79,9 @@
             this.experimentsTree.Dock = System.Windows.Forms.DockStyle.Fill;
             this.experimentsTree.HideSelection = false;
             this.experimentsTree.Location = new System.Drawing.Point(0, 0);
+            this.experimentsTree.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.experimentsTree.Name = "experimentsTree";
-            this.experimentsTree.Size = new System.Drawing.Size(173, 557);
+            this.experimentsTree.Size = new System.Drawing.Size(201, 643);
             this.experimentsTree.TabIndex = 4;
             // 
             // experimentDetails
@@ -90,30 +93,43 @@
             this.experimentDetails.Controls.Add(this.pageSoil);
             this.experimentDetails.Dock = System.Windows.Forms.DockStyle.Fill;
             this.experimentDetails.Location = new System.Drawing.Point(0, 0);
+            this.experimentDetails.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.experimentDetails.Name = "experimentDetails";
             this.experimentDetails.SelectedIndex = 0;
-            this.experimentDetails.Size = new System.Drawing.Size(651, 557);
+            this.experimentDetails.Size = new System.Drawing.Size(760, 643);
             this.experimentDetails.TabIndex = 3;
             // 
             // pageSummary
             // 
             this.pageSummary.BackColor = System.Drawing.Color.LightGray;
             this.pageSummary.Controls.Add(this.summariser);
-            this.pageSummary.Location = new System.Drawing.Point(4, 22);
+            this.pageSummary.Location = new System.Drawing.Point(4, 24);
+            this.pageSummary.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pageSummary.Name = "pageSummary";
-            this.pageSummary.Padding = new System.Windows.Forms.Padding(3);
-            this.pageSummary.Size = new System.Drawing.Size(643, 531);
+            this.pageSummary.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageSummary.Size = new System.Drawing.Size(752, 615);
             this.pageSummary.TabIndex = 7;
             this.pageSummary.Text = "Summary";
+            // 
+            // summariser
+            // 
+            this.summariser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.summariser.Location = new System.Drawing.Point(4, 3);
+            this.summariser.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.summariser.MinimumSize = new System.Drawing.Size(743, 0);
+            this.summariser.Name = "summariser";
+            this.summariser.Size = new System.Drawing.Size(744, 609);
+            this.summariser.TabIndex = 0;
             // 
             // pageDesign
             // 
             this.pageDesign.Controls.Add(this.designBox);
             this.pageDesign.Controls.Add(this.designData);
-            this.pageDesign.Location = new System.Drawing.Point(4, 22);
+            this.pageDesign.Location = new System.Drawing.Point(4, 24);
+            this.pageDesign.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pageDesign.Name = "pageDesign";
-            this.pageDesign.Padding = new System.Windows.Forms.Padding(3);
-            this.pageDesign.Size = new System.Drawing.Size(643, 531);
+            this.pageDesign.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageDesign.Size = new System.Drawing.Size(752, 615);
             this.pageDesign.TabIndex = 8;
             this.pageDesign.Text = "Design";
             this.pageDesign.UseVisualStyleBackColor = true;
@@ -123,9 +139,10 @@
             this.designBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.designBox.Location = new System.Drawing.Point(0, 0);
+            this.designBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.designBox.Name = "designBox";
             this.designBox.ReadOnly = true;
-            this.designBox.Size = new System.Drawing.Size(637, 20);
+            this.designBox.Size = new System.Drawing.Size(744, 23);
             this.designBox.TabIndex = 26;
             // 
             // designData
@@ -134,84 +151,83 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.designData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.designData.Location = new System.Drawing.Point(0, 26);
+            this.designData.Location = new System.Drawing.Point(0, 30);
+            this.designData.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.designData.Name = "designData";
-            this.designData.Size = new System.Drawing.Size(637, 502);
+            this.designData.Size = new System.Drawing.Size(745, 581);
             this.designData.TabIndex = 24;
             // 
             // pageOperation
             // 
             this.pageOperation.BackColor = System.Drawing.Color.LightGray;
             this.pageOperation.Controls.Add(this.operations);
-            this.pageOperation.Location = new System.Drawing.Point(4, 22);
+            this.pageOperation.Location = new System.Drawing.Point(4, 24);
+            this.pageOperation.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pageOperation.Name = "pageOperation";
-            this.pageOperation.Padding = new System.Windows.Forms.Padding(3);
-            this.pageOperation.Size = new System.Drawing.Size(643, 531);
+            this.pageOperation.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageOperation.Size = new System.Drawing.Size(752, 615);
             this.pageOperation.TabIndex = 3;
             this.pageOperation.Text = "Operations";
+            // 
+            // operations
+            // 
+            this.operations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.operations.Location = new System.Drawing.Point(4, 3);
+            this.operations.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.operations.Name = "operations";
+            this.operations.Size = new System.Drawing.Size(744, 609);
+            this.operations.TabIndex = 0;
             // 
             // pageData
             // 
             this.pageData.BackColor = System.Drawing.Color.LightGray;
             this.pageData.Controls.Add(this.traitChart);
-            this.pageData.Location = new System.Drawing.Point(4, 22);
+            this.pageData.Location = new System.Drawing.Point(4, 24);
+            this.pageData.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pageData.Name = "pageData";
-            this.pageData.Padding = new System.Windows.Forms.Padding(3);
-            this.pageData.Size = new System.Drawing.Size(643, 531);
+            this.pageData.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageData.Size = new System.Drawing.Size(752, 615);
             this.pageData.TabIndex = 5;
             this.pageData.Text = "Data";
-            // 
-            // pageSoil
-            // 
-            this.pageSoil.Controls.Add(this.soilsChart);
-            this.pageSoil.Location = new System.Drawing.Point(4, 22);
-            this.pageSoil.Name = "pageSoil";
-            this.pageSoil.Padding = new System.Windows.Forms.Padding(3);
-            this.pageSoil.Size = new System.Drawing.Size(643, 531);
-            this.pageSoil.TabIndex = 9;
-            this.pageSoil.Text = "Soil";
-            this.pageSoil.UseVisualStyleBackColor = true;
-            // 
-            // operations
-            // 
-            this.operations.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.operations.Location = new System.Drawing.Point(3, 3);
-            this.operations.Name = "operations";
-            this.operations.Size = new System.Drawing.Size(637, 525);
-            this.operations.TabIndex = 0;
             // 
             // traitChart
             // 
             this.traitChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.traitChart.Location = new System.Drawing.Point(3, 3);
+            this.traitChart.Location = new System.Drawing.Point(4, 3);
+            this.traitChart.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.traitChart.Name = "traitChart";
-            this.traitChart.Size = new System.Drawing.Size(637, 525);
+            this.traitChart.Size = new System.Drawing.Size(744, 609);
             this.traitChart.TabIndex = 0;
+            // 
+            // pageSoil
+            // 
+            this.pageSoil.Controls.Add(this.soilsChart);
+            this.pageSoil.Location = new System.Drawing.Point(4, 24);
+            this.pageSoil.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageSoil.Name = "pageSoil";
+            this.pageSoil.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pageSoil.Size = new System.Drawing.Size(752, 615);
+            this.pageSoil.TabIndex = 9;
+            this.pageSoil.Text = "Soil";
+            this.pageSoil.UseVisualStyleBackColor = true;
             // 
             // soilsChart
             // 
             this.soilsChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.soilsChart.Location = new System.Drawing.Point(3, 3);
+            this.soilsChart.Location = new System.Drawing.Point(4, 3);
+            this.soilsChart.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.soilsChart.Name = "soilsChart";
-            this.soilsChart.Size = new System.Drawing.Size(637, 525);
+            this.soilsChart.Size = new System.Drawing.Size(744, 609);
             this.soilsChart.TabIndex = 0;
-            // 
-            // experimentSummariser1
-            // 
-            this.summariser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.summariser.Location = new System.Drawing.Point(3, 3);
-            this.summariser.MinimumSize = new System.Drawing.Size(637, 0);
-            this.summariser.Name = "experimentSummariser1";
-            this.summariser.Size = new System.Drawing.Size(637, 525);
-            this.summariser.TabIndex = 0;
             // 
             // ExperimentDetailer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "ExperimentDetailer";
-            this.Size = new System.Drawing.Size(828, 557);
+            this.Size = new System.Drawing.Size(966, 643);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
