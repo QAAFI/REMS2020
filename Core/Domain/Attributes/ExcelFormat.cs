9a075578565a87044ba32b6428ecdb5ef6db8d5c
Michael Stower <michaelostower@gmail.com>
2021-06-28 12:26:45 +1000

    Changed name of property

--- Core/Domain/Attributes/ExcelFormat.cs
+++ Core/Domain/Attributes/ExcelFormat.cs
@@ -7,16 +7,16 @@ namespace Rems.Domain.Attributes
     {
         public string Format { get; private set; }
 
-        public int Priority { get; private set; }
+        public int Dependency { get; private set; }
 
         public bool Required { get; private set; }
 
         public string[] Names { get; private set; }
 
-        public ExcelFormat(string format, int priority, bool required, params string[] names)
+        public ExcelFormat(string format, int dependency, bool required, params string[] names)
         {
             Format = format;
-            Priority = priority;
+            Dependency = dependency;
             Required = required;
             Names = names;
         }
