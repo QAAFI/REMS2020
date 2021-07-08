e75a4c3e92940462847de9a80d53768a9b5e10db
Michael Stower <michaelostower@gmail.com>
2021-07-08 13:41:36 +1000

    Added more entity-column mappings

--- Core/Domain/Entities/SoilLayer.cs
+++ Core/Domain/Entities/SoilLayer.cs
@@ -1,6 +1,5 @@
 ﻿using Rems.Domain.Attributes;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rems.Domain.Entities
 {
@@ -16,13 +15,13 @@ namespace Rems.Domain.Entities
 
         public int SoilId { get; set; }
 
-        [Expected("FromDepth", "DepthFrom")]
+        [Expected("FromDepth", "DepthFrom", "From Depth", "Depth From")]
         public int? FromDepth { get; set; }
 
-        [Expected("ToDepth", "DepthTo")]
+        [Expected("ToDepth", "DepthTo", "To Depth", "Depth To")]
         public int ToDepth { get; set; }
 
-        [Expected("Soil", "Soil Name", "SoilType")]
+        [Expected("Soil", "Soil Name", "SoilType", "Soil Type")]
         public virtual Soil Soil { get; set; }
 
         public virtual ICollection<SoilLayerTrait> SoilLayerTraits { get; set; }
