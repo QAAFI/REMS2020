fbc62378cf6a1e5629de9421726e0221b04aa37d
Mike Stower <michaelostower@gmail.com>
2021-07-30 11:19:05 +1000

    Other REMS .dll's are now signed by the publish script

--- publish.bat
+++ publish.bat
@@ -13,13 +13,18 @@ rem dotnet publish -c Release -f .net5.0-windows -r win-x64 --no-self-contained 
 dotnet publish -v:q -c Release -f net5.0-windows -r win-x64 --no-self-contained -o %REMS% "%~dp0Presentation\WindowsClient\WindowsClient.csproj"
 if errorlevel 1 exit /b 1
 
+set TIMESTAMP="http://timestamp.comodoca.com/?td=sha256"
+rem Sign the .dll's
+SignTool sign -q -as -fd sha256 -tr %TIMESTAMP% -td sha256 -f %CERT% %REMS%\Rems.Domain.dll
+SignTool sign -q -as -fd sha256 -tr %TIMESTAMP% -td sha256 -f %CERT% %REMS%\Rems.Application.dll
+SignTool sign -q -as -fd sha256 -tr %TIMESTAMP% -td sha256 -f %CERT% %REMS%\Persistence.dll
+
 rem Create the installer.
 set "INSTALLER=REMS2020Setup"
 iscc -Q -O%REMS% -F%INSTALLER% -DOUTPUT=%REMS% rems.iss
 if errorlevel 1 exit /b 1
 
 rem Sign the installer
-set TIMESTAMP="http://timestamp.comodoca.com/?td=sha256"
 SignTool sign -q -as -fd sha256 -tr %TIMESTAMP% -td sha256 -f %CERT% %REMS%\%INSTALLER%.exe
 if errorlevel 1 exit /b 1
 SignTool verify -pa -v -d %REMS%\%INSTALLER%.exe
