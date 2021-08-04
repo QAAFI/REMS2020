4783537aaf79250d7db3eda99d3ae7b099646ec5
Michael Stower <michaelostower@gmail.com>
2021-08-04 13:53:40 +1000

    Cleaned up duplicate code

--- Presentation/WindowsClient/Controls/Experiments/Charts/TraitChart.cs
+++ Presentation/WindowsClient/Controls/Experiments/Charts/TraitChart.cs
@@ -148,7 +148,50 @@ namespace WindowsClient.Controls
 
         public virtual async Task LoadPlots()
         {
-            await Task.Delay(50).TryRun();
-        }    
+            string xtitle = "";
+            string ytitle = "";
+
+            List<SeriesData<DateTime, double>> datas = new();
+            Action<SeriesData<DateTime, double>> action = data =>
+            {
+                datas.Add(data);
+                xtitle = data.XName;
+                ytitle = data.YName;
+            };
+
+            if (selected.ToString() == "All")
+                foreach (var plot in await QueryManager.Request(new PlotsQuery { TreatmentId = Treatment }))
+                    await new PlotDataByTraitQuery
+                    {
+                        TraitName = "",
+                        PlotId = plot.Key
+                    }.IterateTraits(traits, action);
+
+            else if (selected.ToString() == "Mean")
+                await new MeanCropTraitDataQuery
+                {
+                    TraitName = "",
+                    TreatmentId = Treatment
+                }.IterateTraits(traits, action);
+
+            else if (selected is PlotDTO plot)
+                await new PlotDataByTraitQuery
+                {
+                    TraitName = "",
+                    PlotId = plot.ID
+                }.IterateTraits(traits, action);
+
+            chart.Series.Clear();
+            datas.ForEach(d => d.AddToSeries(chart.Series));
+
+            chart.Axes.Bottom.Title.Text = xtitle;
+            chart.Axes.Left.Title.Text = ytitle;
+            chart.Legend.Title.Text = descriptions[0]?.WordWrap(18) ?? "";
+
+            chart.Legend.Width = 120;
+
+            chart.Header.Text = await QueryManager.Request(new TreatmentDesignQuery { TreatmentId = Treatment });
+
+        }
     }
 }
