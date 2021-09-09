b1a2cece770793f32e396aafe887f0d8fac28fde
Michael Stower <michaelostower@gmail.com>
2021-09-09 15:12:25 +1000

    Added a reworked version of SoilLayerData querying
    The initial version of querying series data for charts performed a single query for each series, and returned them inside a custom SeriesData object. Additionally, there was a separate query for the mean data.
    
    The new approach returns the data for each plot in a DataTable, with one column per plot/repetition, and an additional plot for the average. This requires far fewer queries (1 per trait/per treatment instead of 1 per plot/per treatment) and as a result should be much faster.
    
    Additionally, the intent is each trait table is saved to a DataSet the first time it is called. This will further reduce the number of queries made, at the cost of additional memory.

---
+++ Core/Application/CQRS/Controls/Charts/SoilLayerDataQuery.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Rems.Application.Common;
+using Rems.Application.Common.Interfaces;
+
+namespace Rems.Application.CQRS
+{
+    /// <summary>
+    /// Finds the average value of a soil trait in a treatment for a given date
+    /// </summary>
+    public class SoilLayerDataQuery : ContextQuery<DataTable>
+    {
+        /// <summary>
+        /// The source treatment
+        /// </summary>
+        public int TreatmentId { get; set; }
+
+        public string TraitName { get; set; }
+
+        /// <summary>
+        /// The date
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <inheritdoc/>
+        public class Handler : BaseHandler<SoilLayerDataQuery>
+        {
+            public Handler(IRemsDbContextFactory factory) : base(factory) { }
+        }
+
+        /// <inheritdoc/>
+        protected override DataTable Run()
+        {
+            var table = new DataTable($"{TreatmentId}_{TraitName}_{Date}");
+
+            var tmt = _context.Treatments.Find(TreatmentId);
+
+            // Add columns
+            table.Columns.Add("Depth", typeof(int));
+            foreach (var plot in tmt.Plots)
+                table.Columns.Add(plot.Repetition.ToString(), typeof(double));
+            table.Columns.Add("Mean", typeof(double));
+
+            // Find the data
+            var depths = _context.SoilLayerDatas
+                .Where(p => p.Plot.TreatmentId == TreatmentId)
+                .Where(p => p.Trait.Name == TraitName)                
+                .Where(p => p.Date == Date)
+                .ToArray()
+                .GroupBy(p => p.DepthFrom)
+                .OrderBy(g => g.Key)
+                .ToArray();
+
+            // Add rows
+            foreach (var depth in depths)
+            {
+                var row = table.NewRow();
+                row["Depth"] = depth.Key;
+
+                foreach (var data in depth)
+                    row[$"{data.Plot.Repetition}"] = data.Value;
+                
+                row["Mean"] = depth.Select(d => d.Value).Average();
+            }            
+
+            return table;
+        }
+    }
+}
