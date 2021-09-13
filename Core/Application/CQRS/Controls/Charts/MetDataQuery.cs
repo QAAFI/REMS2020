55a1ae750074ac2e6aa3d64217363585c9f33f65
Mike Stower <53413135+MikeStower@users.noreply.github.com>
2021-09-13 13:10:12 +1000

    Separated out met chart from other trait charts
    The requirements for querying / displaying met data are different enough to warrant a separate control / query setup to traits. There will be a bit more code, but it will be cleaner / more sensible than simply forcing everything to be as generic as possible.

---
+++ Core/Application/CQRS/Controls/Charts/MetDataQuery.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Rems.Application.Common;
+using Rems.Application.Common.Interfaces;
+using Rems.Domain.Entities;
+
+namespace Rems.Application.CQRS
+{
+    /// <summary>
+    /// Finds the average values for a trait across a treatment
+    /// </summary>
+    public class MetDataQuery : ContextQuery<DataTable>
+    {
+        /// <summary>
+        /// The source treatment
+        /// </summary>
+        public int TreatmentId { get; set; }
+
+        /// <summary>
+        /// The trait
+        /// </summary>
+        public string TraitName { get; set; }
+
+        /// <inheritdoc/>
+        public class Handler : BaseHandler<MetDataQuery>
+        {
+            public Handler(IRemsDbContextFactory factory) : base(factory) { }
+        }
+
+        /// <inheritdoc/>
+        protected override DataTable Run()
+        {   
+            // Check sensibility
+            if (_context.Traits.FirstOrDefault(t => t.Name == TraitName) is not Trait trait)
+                return null;
+            
+            // Find the data
+            var tmt = _context.Treatments.Find(TreatmentId);
+
+            var data = tmt.Experiment.MetStation.MetData
+                .Where(p => p.Trait.Name == TraitName)
+                .GroupBy(m => (m.Date.Day, m.Date.Month))
+                .Select(g => (new DateTime(2020, g.Key.Month, g.Key.Day), g.Select(m => m.Value).Average()));
+
+            // Construct the table
+            var table = new DataTable($"{TreatmentId}_{TraitName}");            
+
+            table.Columns.Add("Date", typeof(DateTime));
+            table.Columns.Add("Mean", typeof(double));
+
+            // Populate the table
+            foreach (var point in data)
+            {
+                var row = table.NewRow();
+                row["Date"] = point.Item1;
+                row["Mean"] = point.Item2;
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+    }
+}
