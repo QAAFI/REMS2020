7161fc8ad388e01ce21036b17a4c8ecdb89d3843
Mike Stower <michaelostower@gmail.com>
2021-10-27 15:19:05 +1000

    Created ReportTemplate
    Working on #262

---
+++ Core/Infrastructure/ApsimX/Templates/ReportTemplate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Rems.Application.Common.Interfaces;
+using Rems.Application.CQRS;
+using Models;
+
+namespace Rems.Infrastructure.ApsimX
+{
+    public class ReportTemplate : ITemplate<Report>
+    {
+        readonly IQueryHandler Handler;
+
+        readonly IFileManager Manager;
+
+        readonly int ID;
+
+        public ReportTemplate(int id, IQueryHandler handler, IFileManager manager)
+        {
+            ID = id;
+            Handler = handler;
+            Manager = manager;
+        }
+
+        public Report Create() => AsyncCreate().Result;
+
+        public async Task<Report> AsyncCreate()
+        {
+            var crop = await Handler.Query(new CropQuery { ExperimentId = ID });
+            var info = Manager.GetFileInfo($"{crop}Daily") ?? Manager.GetFileInfo("DefaultDaily");
+
+            return JsonTools.LoadJson<Report>(info);
+        }
+    }
+}
