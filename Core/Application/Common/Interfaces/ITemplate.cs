5aea61bb3aa5b89b1a929e14db180463dacc86ea
Mike Stower <michaelostower@gmail.com>
2021-10-27 14:58:25 +1000

    Created Template interface
    Resolves #261

---
+++ Core/Application/Common/Interfaces/ITemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Rems.Application.Common.Interfaces
+{
+    /// <summary>
+    /// A template for creating an instance of a class
+    /// </summary>
+    public interface ITemplate<T> where T : class
+    {
+        public T Create();
+
+        public Task<T> AsyncCreate();
+    }
+}
